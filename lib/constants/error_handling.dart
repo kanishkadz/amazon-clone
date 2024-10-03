5638e186e82bef0f9f99c2dc2836acf89d5b8c2e
Kanishka Anand <kanishkaadz@gmail.com>
2024-10-03 21:40:54 +0530

    Create error_handling.dart

---
+++ lib/constants/error_handling.dart
@@ -0,0 +1,9 @@
+import 'dart:convert';
+import 'package:amazon_clone/constants/utils.dart';
+import 'package:flutter/material.dart';
+import 'package:http/https.dart' as http;
+
+void httpErrorHandle({
+  required https.Response response,
+  required
+})
